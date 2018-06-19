c13875777bd23a103d5f9a58bd0555230d2c98f0
Michael Lustenberger <mic@inofix.ch>
2018-06-19 13:06:19 +0200

    JSON Backend
    Add a first step towards the backend, a JSON store that can later be
    saved to resp. loaded from the db.

--- sketch-board-web/src/main/resources/META-INF/resources/js/index.js
+++ sketch-board-web/src/main/resources/META-INF/resources/js/index.js
@@ -3,11 +3,63 @@ import * as d3 from 'd3/dist/d3';
 export default function(portletNamespace, json) {
     Liferay.Loader.require('sketch-board-web@1.0.0/js/noteit.board.es', function (noteit) {
 
-        let board = new noteit.SketchBoard(`#${portletNamespace}-mainSVG`);
-        let rstack = new noteit.Stack(board, board.group, "Type", ["#f7ff72", "#ff6ee2", "#6ee0ff"], [14,14]);
-        if (typeof json !== 'undefined' && json.length > 3 ) {
-            board.fromJSON(json);
+        function createBoard(json) {
+            board = new noteit.SketchBoard(`#${portletNamespace}-mainSVG`);
+            let s = new noteit.Stack(board, board.group,
+                        "Type", ["#f7ff72", "#ff6ee2", "#6ee0ff"], [14,14]);
+            if (typeof json !== 'undefined' && json.length > 3 ) {
+                board.fromJSON(json);
+            }
         }
+
+        function createTextarea(json) {
+            store = storeDiv.append("textarea").attr("rows", 100)
+                                    .attr("cols", 80).attr("value", json);
+            store.text(json);
+        }
+
+        var board;
+        createBoard(json);
+
+        var boardDiv = d3.select(`#${portletNamespace}-board`);
+        boardDiv.style("height", "100%");
+        boardDiv.style("width", "100%");
+        var storeDiv = d3.select(`#${portletNamespace}-json`);
+        storeDiv.style("visibility", "hidden");
+
+        storeDiv.style("height", "0%");
+        storeDiv.style("width", "100%");
+        var store = null;
+
+        var curOutputJSON = false;
+        let switchButtonDiv = d3.select(`#${portletNamespace}-button`);
+        var switchButton = switchButtonDiv.append("input")
+                    .attr("type", "button").attr("value", "JSON")
+                    .on("click", function() {
+            curOutputJSON = !curOutputJSON;
+
+            if (curOutputJSON === true) {
+                switchButton.attr("value", "JSON");
+                storeDiv.style("visibility", "visible");
+                storeDiv.style("height", "100%");
+                boardDiv.style("visibility", "hidden");
+//                boardDiv.style("height", "0%");
+                switchButton.attr("value", "SVG");
+//                store.text(board.toJSON("  "));
+                createTextarea(board.toJSON("  "));
+                board.group.remove();
+                board = null;
+            } else {
+                switchButton.attr("value", "SVG");
+                storeDiv.style("visibility", "hidden");
+                storeDiv.style("height", "0%");
+                boardDiv.style("visibility", "visible");
+//                boardDiv.style("height", "100%");
+                switchButton.attr("value", "JSON");
+                createBoard(store.node().value);
+                store.remove();
+            }
+        });
     });
 }
 
--- sketch-board-web/src/main/resources/META-INF/resources/view.jsp
+++ sketch-board-web/src/main/resources/META-INF/resources/view.jsp
@@ -1,8 +1,11 @@
 <%@ include file="/init.jsp" %>
 
-<div id="<portlet:namespace />-json"></div>
-<div id="<portlet:namespace />-board" class="board">
-<svg id="<portlet:namespace />-mainSVG" width="100%" height="2000px"></svg>
+<div id="<portlet:namespace />-all" width="100%" height="2000px">
+    <div id="<portlet:namespace />-button"></div>
+    <div id="<portlet:namespace />-json"></div>
+    <div id="<portlet:namespace />-board" class="board">
+        <svg id="<portlet:namespace />-mainSVG" width="100%" height="2000px"></svg>
+    </div>
 </div>
 
 <aui:script require="sketch-board-web@1.0.0">
