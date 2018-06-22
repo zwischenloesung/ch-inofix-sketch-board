82575cc45239e883fa59b004ee0d5aad746d990d
Michael Lustenberger <mic@inofix.ch>
2018-06-22 23:55:09 +0200

    Add Header

--- sketch-board-web/src/main/java/ch/inofix/sketchboard/constants/SketchBoardPortletKeys.java
+++ sketch-board-web/src/main/java/ch/inofix/sketchboard/constants/SketchBoardPortletKeys.java
@@ -1,7 +1,10 @@
 package ch.inofix.sketchboard.constants;
 
 /**
- * @author mic
+ * @author Michael Lustenberger
+ * @create 2018-06-22
+ * @modified 2018-06-22
+ * @version 1.0.0
  */
 public class SketchBoardPortletKeys {
 
